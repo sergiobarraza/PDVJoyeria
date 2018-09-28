9764202478720c72a30e4e16d198f97b22a00be8
Daniel Rocha <danielrocha94@msn.com>
2018-09-28 00:44:52 -0500

    Search persona acepta RFC como parametro de busqueda

--- pdv2/search_person.php
+++ pdv2/search_person.php
@@ -7,39 +7,48 @@
   ini_set('display_startup_errors', 1);
   error_reporting(E_ALL);
 
+  $searchq;
+  $sql;
+
   if(isset($_POST['tel'])) {
-    try {
-      $searchq = $_POST['tel'];
-      $searchq = preg_replace("/\D/", '', $searchq);
+    $searchq = $_POST['tel'];
+    $searchq = preg_replace("/\D/", '', $searchq);
 
-      $sql_tel = "SELECT * FROM Persona WHERE tel = '$searchq' ";
+    $sql = "SELECT * FROM Persona WHERE tel = '$searchq' ";
 
-      $statement = $connection->prepare($sql_tel);
-      $statement->bindValue(1, "%$searchq%", PDO::PARAM_STR);
-      $seached_person = $statement->execute();
+  } else if ($_POST['rfc']) {
+    $searchq = $_POST['rfc'];
+    $searchq = preg_replace("/\s+/", '', $searchq);
 
-      $data;
+    $sql = "SELECT * FROM Persona WHERE rfc = '$searchq' ";
+  }
 
-      if(!$statement->rowCount() == 0) {
-        while($row = $statement->fetch()){
-          $data = [
-            'id' => $row['idPersona'],
-            'nombre' => $row['nombre'],
-            'apellido' => $row['apellido'],
-            'rfc' => $row['rfc'],
-            'tel' => $row['tel']
-          ];
-        }
-      } else {
-        echo "No se encontro informacion.";
+  try {
+    $statement = $connection->prepare($sql);
+    $statement->bindValue(1, "%$searchq%", PDO::PARAM_STR);
+    $seached_person = $statement->execute();
+
+    $data;
+
+    if(!$statement->rowCount() == 0) {
+      while($row = $statement->fetch()){
+        $data = [
+          'id' => $row['idPersona'],
+          'nombre' => $row['nombre'],
+          'apellido' => $row['apellido'],
+          'rfc' => $row['rfc'],
+          'tel' => $row['tel']
+        ];
       }
-
-      header('Content-type: application/json');
-      echo json_encode( $data );
-
-    } catch(PDOException $error) {
-      echo $sql . "<br>" . $error->getMessage();
+    } else {
+      echo "No se encontro informacion.";
     }
+
+    header('Content-type: application/json');
+    echo json_encode( $data );
+
+  } catch(PDOException $error) {
+    echo $sql . "<br>" . $error->getMessage();
   }
 
 ?>
